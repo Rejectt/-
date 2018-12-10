a06730be60a2d83e02335439a6599c58fb3cfb1a
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-12-10 06:45:18 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1801,6 +1801,25 @@ message.guild.createChannel('Talk ◥', 'voice')
     }
 });
 
+client.on("message", message => {
+  if(message.content.startsWith("!verify")) { // الامر والبريفكس
+    let num = Math.floor((Math.random() * 4783) + 10);
+ 
+    message.channel.send(`يرجاء كتابة الرقم التالي: **${num}**`).then(m => {
+      message.channel.awaitMessages(res => res.content == `${num}`, {
+        max: 1,
+        time: 60000,
+        errors: ['time'],
+      }).then(collected => {
+        message.delete();
+        m.delete();
+        message.member.addRole(message.guild.roles.find(c => c.name == "active")); // اسم الرتبة
+      }).catch(() => {
+        m.edit(`You took to long to type the number.\nRe-type the command again if you want to verify yourself.`).then(m2 => m.delete(15000));
+      });
+    });
+  }
+});
 
 
 
