105123836be86442ec107cb3f93ee587b7c88172
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-12-11 05:23:16 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1915,7 +1915,6 @@ client.on('message', function(message) {
 const fs = require('fs'); // npm i fs
 const ms = require('ms'); // npm i ms
 const cool = [];
-hero.on('message',async message => {
   if(message.author.bot) return;
   if(message.channel.type === 'dm') return;
 
